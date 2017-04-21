3faf5dfbcceea20e97d12626a1a889a2e462b67c
gitcolt <colten42@gmail.com>
2017-04-21 17:00:19 -0500

    Pass all tests in 00, 01, and 002

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,27 @@
-#write your code here
+def add(a, b)
+  a + b
+end
+
+def subtract(a, b)
+  a - b
+end
+
+def sum(array)
+  array.reduce(0, :+)
+end
+
+def multiply(array)
+  array.reduce(1, :*)
+end
+
+def power(x, pow)
+  x ** pow
+end
+
+def factorial(x)
+  if x == 0
+    return 1
+  else
+    return x * factorial(x - 1)
+  end
+end
