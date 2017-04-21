eb5cc48e021f1289e75c966ea275ab61eb26e577
gitcolt <colten42@gmail.com>
2017-04-21 18:20:23 -0500

    Pass tests 00-04

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,9 @@
-#write your code here
+#copied from official solutions
+def translate(message)
+  message.split.map do |word|
+        word =~ /^([^aeiouyq]*(qu)?)(.*)$/x
+        first_phoneme = $1
+        rest_of_word = $3
+        "#{rest_of_word}#{first_phoneme}ay"
+  end.join(" ")
+end
