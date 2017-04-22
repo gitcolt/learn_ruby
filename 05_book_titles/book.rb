360176bd9796e67776991824227bb99ce0ac950a
Colt <colten@utexas.edu>
2017-04-22 21:30:51 +0000

    Pass tests 00-05

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,16 @@
 class Book
-# write your code here
+  attr_reader :title
+  @@little_words = %w(a an the and but or for nor on at to from by in of)
+
+  def title=(new_title)
+    words = new_title.split.map do |word|
+      if @@little_words.include?(word)
+        word
+      else
+        word.capitalize
+      end
+    end
+    words.first.capitalize!
+    @title = words.join(" ");
+  end
 end
